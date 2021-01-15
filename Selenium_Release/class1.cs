a93e074718afc445accad14b60b5312c5a81a62b
anuragpatidar17 <anurag.patidar@yahoo.in>
2021-01-15 09:25:37 +0530

    first commit

---
+++ Selenium_Release/class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NPOI.SS.UserModel;
+using System.Threading;
+using NPOI.Util;
+using NPOI.XSSF.UserModel;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+using Assert = NUnit.Framework.Assert;
+
+
+namespace Azure_demo
+{
+
+    public class class1
+    {
+        [Test]
+        public void SearchForWord()
+        {
+            var driver = new ChromeDriver();
+
+            {
+              
+                driver.Navigate().GoToUrl("https://google.com");
+
+                Console.WriteLine(driver.Title + " coming from test 1,hello ");
+
+                driver.Quit();
+               
+
+
+            }
+        }
+    }
+}
